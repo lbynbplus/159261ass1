e8939d9bdcc9b8e104d934616aec058a1e104a52
lbynbplus <lvboyu2001@gmail.com>
2023-04-23 12:57:25 +1200

    s

---
+++ src/Homepage.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class Homepage extends GameEngine {
+
+    public static void main(String[] args) {
+        Homepage game = new Homepage();
+        game.setupWindow(800, 600);
+    }
+
+    public void initGame() {
+        // 初始化游戏
+    }
+
+    public void update(double dt) {
+        // 更新游戏状态
+    }
+
+
+    public void paintComponent() {
+
+    }
+
+    public void draw(Graphics2D g) {
+        // 绘制游戏界面
+    }
+
+    public void keyPressed(KeyEvent e) {
+        // 处理按键事件
+    }
+
+    public void keyReleased(KeyEvent e) {
+        // 处理释放按键事件
+    }
+
+    public void keyTyped(KeyEvent e) {
+        // 处理输入字符事件
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        // 处理鼠标单击事件
+    }
+
+    public void mousePressed(MouseEvent e) {
+        // 处理鼠标按下事件
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        // 处理鼠标释放事件
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        // 处理鼠标进入事件
+    }
+
+    public void mouseExited(MouseEvent e) {
+        // 处理鼠标离开事件
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        // 处理鼠标拖拽事件
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        // 处理鼠标移动事件
+    }
+}
